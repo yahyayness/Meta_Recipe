3ebfe1628d01fa75e4396f4d397266bd6108a3f4
Khalid Sabbah <khalidsami.se@gmail.com>
2023-05-10 10:17:13 +0300

    create user api ..

--- UnitOne/aj_dashboard/backend/users/__serializers/UserSerializer.py
+++ UnitOne/aj_dashboard/backend/users/__serializers/UserSerializer.py
@@ -4,7 +4,21 @@ from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length=128,
+        min_length=6,
+        write_only=True,
+        required=False
+    )
+    email = serializers.EmailField(required=True)
+    username = serializers.CharField(max_length=70, required=True)
+    first_name = serializers.CharField(max_length=20, required=True)
+    last_name = serializers.CharField(max_length=20, required=True)
+
     class Meta:
         model = User
         fields = ('id', 'username', 'password', 'email', 'first_name', 'last_name')
         extra_kwargs = {'password': {'write_only': True, 'required': False}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)

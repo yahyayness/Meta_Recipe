96a478102611566f7d4606f2a0929d13c2cfa527
Amr M. Saidam <sc.amr.2017@gmail.com>
2023-05-11 19:26:46 +0300

    working on the login

--- UnitOne/aj_dashboard/backend/users/models.py
+++ UnitOne/aj_dashboard/backend/users/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     class Meta:
-        # managed = False
+        managed = False
         db_table = 'auth_user'
--- UnitOne/aj_dashboard/frontend/src/App.tsx
+++ UnitOne/aj_dashboard/frontend/src/App.tsx
@@ -2,16 +2,43 @@ import React, {useState} from 'react';
 import './App.scss'
 import AppLayout from "./layout";
 import AppRoutes from "./routes";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import AuthLayout from "./layout/auth";
 
+const Layouts = {
+    auth : <AuthLayout/>,
+    app: <AppLayout/>
+}
 /**
  * application start point
  * @constructor
  * @author Amr
  */
 const App: React.FC = () => {
+
+    const [currentLayout , setCurrentLayout] = useState<any>(null)
+    const location = useLocation()
+
+    useEffect(() => {
+        const path = location.pathname;
+        if(path?.includes('auth')){
+            console.log(`Route changed to ${location.pathname}`);
+            setCurrentLayout(Layouts.auth)
+        }
+
+        else
+            setCurrentLayout(Layouts.app)
+        console.log(`Route changed to ${location.pathname}`);
+        // Perform other actions based on the route change
+    }, [location?.pathname]);
     return (
         <>
-            <AppLayout/>
+            {
+                currentLayout && React.cloneElement(currentLayout)
+            }
+            <AppRoutes/>
+            {/*<AppLayout/>*/}
         </>
 
 

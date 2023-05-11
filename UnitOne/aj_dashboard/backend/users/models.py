d138942624e01d7c8547a1560857f2605c7cc89f
Khalid Sabbah <khalidsami.se@gmail.com>
2023-05-11 11:09:53 +0300

    fix user migration ..

--- UnitOne/aj_dashboard/backend/users/models.py
+++ UnitOne/aj_dashboard/backend/users/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     class Meta:
         # managed = False
-        db_table = 'auth_user'
+        db_table = 'recipe_users'

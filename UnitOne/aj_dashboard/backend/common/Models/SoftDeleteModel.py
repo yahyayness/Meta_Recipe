11b7710dc3a7fc09ba7aac25253a9a3bdc6d0d5d
Khalid Sabbah <khalidsami.se@gmail.com>
2023-05-15 15:23:41 +0300

    new SoftDeleteModel

---
+++ UnitOne/aj_dashboard/backend/common/Models/SoftDeleteModel.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+from common.utilities.SoftDeleteManager import SoftDeleteManager
+
+
+class SoftDeleteModel(models.Model):
+    deleted_at = models.DateTimeField(null=True, default=None)
+    objects = SoftDeleteManager()
+    all_objects = models.Manager()
+
+    def soft_delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def restore(self):
+        self.deleted_at = None
+        self.save()
+
+    class Meta:
+        abstract = True

31bf1b9927d2ea14b29b9d299f8a4e1b971799a4
Khalid Sabbah <khalidsami.se@gmail.com>
2023-05-17 15:20:48 +0300

    update Protocol model - add new ProtocolProcess model - update Recipe model fields

--- UnitOne/aj_dashboard/backend/recipe/models.py
+++ UnitOne/aj_dashboard/backend/recipe/models.py
@@ -2,13 +2,19 @@ from django.db import models
 from django.db.models import Model
 
 from meta_recipe.models import MetaRecipe
+from protocols.models import Protocol
 
 
 # Create your models here.
 
 class Recipe(Model):
-    meta_recipe = models.ForeignKey(MetaRecipe, on_delete=models.CASCADE, related_name='recipe',blank=False, null=False)
-    amount = models.FloatField(null=False, default=0)
+    meta_recipe = models.ForeignKey(MetaRecipe, on_delete=models.CASCADE, related_name='recipes_for_meta', blank=False,
+                                    null=False)
+    name = models.CharField(unique=True, blank=False, null=False)
+    protocol = models.ForeignKey(Protocol, on_delete=models.CASCADE, related_name='recipes_for_protocol', blank=False,
+                                 null=False)
+
+    # amount = models.FloatField(null=False, default=0)
 
     class Meta:
         db_table = 'recipe'
@@ -16,3 +22,4 @@ class Recipe(Model):
 
     def __str__(self):
         return f"{self.meta_recipe.name} : {self.amount}"
+

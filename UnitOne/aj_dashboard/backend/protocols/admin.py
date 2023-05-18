5e4ddcb67d4508d4c220c9dc66e90a53ae4d6869
zbilal23 <125123034+zbilal23@users.noreply.github.com>
2023-05-18 15:54:56 +0300

    [project ] import data

--- UnitOne/aj_dashboard/backend/protocols/admin.py
+++ UnitOne/aj_dashboard/backend/protocols/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Protocol
 # Register your models here.
+
+admin.site.register(Protocol)
--- UnitOne/aj_dashboard/backend/recipe/admin.py
+++ UnitOne/aj_dashboard/backend/recipe/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Recipe,RecipeIngredients
 # Register your models here.
+
+admin.site.register(Recipe)
+admin.site.register(RecipeIngredients)

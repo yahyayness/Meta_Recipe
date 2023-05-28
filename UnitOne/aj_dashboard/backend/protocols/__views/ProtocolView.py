1ca718c46922a992fed069a15acd43f1d475ccf8
Khalid Sabbah <khalidsami.se@gmail.com>
2023-05-28 12:20:46 +0300

    change to hard delete protocol ..

--- UnitOne/aj_dashboard/backend/protocols/__views/ProtocolView.py
+++ UnitOne/aj_dashboard/backend/protocols/__views/ProtocolView.py
@@ -77,7 +77,7 @@ class ProtocolView(GenericViewSet):
 
     def destroy(self, request, pk, *args, **kwargs):
         protocol = Protocol.objects.get(id=pk)
-        protocol.soft_delete()
+        protocol.delete()
         return Response(
             {'status': 'success', 'code': status.HTTP_200_OK, 'message': 'Protocol deleted!', 'payload': {}},
             status=status.HTTP_200_OK
@@ -86,67 +86,69 @@ class ProtocolView(GenericViewSet):
     @action(detail=False, methods=['POST'])
     @transaction.atomic
     def create_flow(self, request, protocol_id=None, *args, **kwargs):
-        protocol = Protocol.objects.get(id=protocol_id)
-        if 'flow' not in request.data:
-            protocol.flow = request.data
-        else:
-            protocol.flow = request.data['flow']
-        protocol.save()
-        protocol.refresh_from_db()
-        nodes = {}
-        if 'nodes' in request.data:
-            for node in request.data['nodes']:
-                if node['type'] == 'ingredient-container':
-                    for child in node['data']['children']:
-                        ing_name = child['data']['value']['name']
-                        ing_amount = child['data']['value']['amount']
-                        ing_unit = 'g'
-                        obj, created = Ingredients.objects.get_or_create(
-                            name=ing_name
-                        )
-                        protocol_ingredient = ProtocolIngredient.objects.create(protocol=protocol,
-                                                                                ingredient=obj,
-                                                                                unit=ing_unit,
-                                                                                quantity=ing_amount)
-                        protocol_node = ProtocolNode.objects.create(protocol=protocol,
-                                                                    model_type='ProtocolIngredient',
-                                                                    model_id=protocol_ingredient.id,
-                                                                    container=node['id'],
-                                                                    slug=child['id'],
-                                                                    type='ingredient',
-                                                                    payload=child['data'])
-                        nodes[child['id']] = protocol_node.id
-                elif node['type'] == 'process':
-                    for child in node['data']['children']:
-                        process = Process.objects.get(name__iexact=node['data']['label'])
-                        protocol_process = ProtocolProcess.objects.create(protocol=protocol,
-                                                                          process=process,
-                                                                          arguments=child['data'],
-                                                                          value=child['data']['value']
-                                                                          )
-                        protocol_node = ProtocolNode.objects.create(protocol=protocol,
-                                                                    model_type='ProtocolProcess',
-                                                                    model_id=protocol_process.id,
-                                                                    container=node['id'],
-                                                                    slug=child['id'],
-                                                                    type='process',
-                                                                    payload=child['data'])
-                        nodes[child['id']] = protocol_node.id
-                elif node['type'] in ['merge', 'serve']:
-                    protocol_node = ProtocolNode.objects.create(protocol=protocol,
-                                                                container=node['id'],
-                                                                slug=node['id'],
-                                                                type=node['type'])
-                    nodes[node['id']] = protocol_node.id
+        try:
+            with transaction.atomic():
+                protocol = Protocol.objects.get(id=protocol_id)
+                flow = {}
+                if 'flow' in request.data:
+                    protocol.flow = request.data['flow']
+                    flow = request.data['flow']
+                else:
+                    protocol.flow = request.data
+                    flow = request.data
+                protocol.save()
+                protocol.refresh_from_db()
+                nodes = {}
+                if 'nodes' in flow:
+                    for node in flow['nodes']:
+                        if node['type'] == 'ingredient-container':
+                            for child in node['data']['children']:
+                                ing_name = child['data']['value']['name']
+                                ing_amount = child['data']['value']['amount']
+                                ing_unit = 'g'
+                                obj, created = Ingredients.objects.get_or_create(
+                                    name=ing_name
+                                )
+                                protocol_ingredient = ProtocolIngredient.objects.create(protocol=protocol,
+                                                                                        ingredient=obj,
+                                                                                        unit=ing_unit,
+                                                                                        quantity=ing_amount)
+                                protocol_node = ProtocolNode.objects.create(protocol=protocol,
+                                                                            model_type='ProtocolIngredient',
+                                                                            model_id=protocol_ingredient.id,
+                                                                            container=node['id'],
+                                                                            slug=child['id'],
+                                                                            type='ingredient',
+                                                                            payload=child['data'])
+                                nodes[child['id']] = protocol_node.id
+                        elif node['type'] == 'process':
+                            for child in node['data']['children']:
+                                process = Process.objects.get(name__iexact=node['data']['label'])
+                                protocol_process = ProtocolProcess.objects.create(protocol=protocol,
+                                                                                  process=process,
+                                                                                  arguments=child['data'],
+                                                                                  value=child['data']['value']
+                                                                                  )
+                                protocol_node = ProtocolNode.objects.create(protocol=protocol,
+                                                                            model_type='ProtocolProcess',
+                                                                            model_id=protocol_process.id,
+                                                                            container=node['id'],
+                                                                            slug=child['id'],
+                                                                            type='process',
+                                                                            payload=child['data'])
+                                nodes[child['id']] = protocol_node.id
+                        elif node['type'] in ['merge', 'serve']:
+                            protocol_node = ProtocolNode.objects.create(protocol=protocol,
+                                                                        container=node['id'],
+                                                                        slug=node['id'],
+                                                                        type=node['type'])
+                            nodes[node['id']] = protocol_node.id
 
-        if 'edges' in request.data:
-            for edge in request.data['edges']:
-                source_id = nodes[edge['sourceHandle'].replace('-source', '')]
-                target_id = nodes[edge['targetHandle'].replace('-target', '')]
-                ProtocolEdge.objects.create(source_node_id=source_id, target_node_id=target_id)
-        return ProtocolSerializer(protocol).data
-        # return Response(
-        #     {'status': 'success', 'code': status.HTTP_200_OK, 'message': 'Protocol Created!',
-        #      'payload': ProtocolSerializer(protocol).data},
-        #     status=status.HTTP_200_OK
-        # )
+                if 'edges' in flow:
+                    for edge in flow['edges']:
+                        source_id = nodes[edge['sourceHandle'].replace('-source', '')]
+                        target_id = nodes[edge['targetHandle'].replace('-target', '')]
+                        ProtocolEdge.objects.create(source_node_id=source_id, target_node_id=target_id)
+                return ProtocolSerializer(protocol).data
+        except Exception as e:
+            raise Exception(str(e))

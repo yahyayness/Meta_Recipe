5c5aa40cb7a9b25c2dcb89078c155f09928359d8
Amr M. Saidam <sc.amr.2017@gmail.com>
2023-05-28 14:08:49 +0300

    fix ingredient's edit problem

--- UnitOne/aj_dashboard/frontend/src/pages/protocols/components/ingredient-row/index.tsx
+++ UnitOne/aj_dashboard/frontend/src/pages/protocols/components/ingredient-row/index.tsx
@@ -8,19 +8,34 @@ import {IngredientType} from "../../../../types/ModelTypes";
 
 const IngredientRow:React.FC<any> = ({ data, isConnectable, index  , onChange})=>{
     const [value , setValue] = useState<IngredientType>((data?.data.value ?? {}) as IngredientType)
+    const [amount , setAmount] = useState(data?.data?.value?.amount ?? '')
+    const [name , setName] = useState(data?.data?.value?.name ?? '')
+    const [unit, setUnit] = useState(data?.data?.value?.unit ?? 'g')
 
+    /**
+     * this object includes the set methods of component's states
+     * so, you can call whatever set method you want by passing the name of state
+     * @param name
+     * @author Amr
+     */
+    const set = (name:string)=> {
+        const setMethods:any =  {
+            'amount' : setAmount,
+            'name' : setName
+        }
+        return  setMethods[name]
+    }
+    /**
+     * handle the changes of component's inputs
+     * @param event
+     * @author Amr
+     */
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>)=> {
-        // value[event.target.name] = event.target.value;
-        setValue(_value => {
-            return {..._value , [event.target.name] :  event.target.value}
-        } )
-        onChange(index ,value);
+        set(event.target.name)( ''+ event.target.value)
+        onChange(index , {name , amount , unit , [event.target.name] : event.target.value });
     }
 
-    const isValidConnection = (connection:any)=>{
-        console.log('connection' , connection)
-        return true;
-    }
+    const isValidConnection = (connection:any)=>  true
 
 
 
@@ -32,8 +47,8 @@ const IngredientRow:React.FC<any> = ({ data, isConnectable, index  , onChange})=
             <Grid item xs={7} width='7ch' >
                 <FormControl sx={{ m: 1}} variant="outlined"   size="small"  >
                     <OutlinedInput
-                        value={value.name}
-                        onChange={handleChange}
+                        value={name}
+                        onInput={handleChange}
                         name='name'
                         id="outlined-adornment-weight"
                         aria-describedby="outlined-weight-helper-text"
@@ -48,11 +63,11 @@ const IngredientRow:React.FC<any> = ({ data, isConnectable, index  , onChange})=
                     <OutlinedInput
                         className={'number-input'}
                         type='number'
-                        value={value.amount}
-                        onChange={handleChange}
+                        value={amount}
+                        onInput={handleChange}
                         name='amount'
                         id="outlined-adornment-weight"
-                        endAdornment={<InputAdornment position="end">g</InputAdornment>}
+                        endAdornment={<InputAdornment position="end">{unit}</InputAdornment>}
                         aria-describedby="outlined-weight-helper-text"
                         inputProps={{
                             'aria-label': 'weight',

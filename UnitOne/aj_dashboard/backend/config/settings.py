efff81b39371f108bf64a3d47ad208eadf4a3da9
Khalid Sabbah <khalidsami.se@gmail.com>
2023-06-18 10:15:06 +0300

    debugging ..

--- UnitOne/aj_dashboard/backend/config/settings.py
+++ UnitOne/aj_dashboard/backend/config/settings.py
@@ -140,7 +140,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

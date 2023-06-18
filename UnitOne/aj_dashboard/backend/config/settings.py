316f88f6b93ee87e2710de0d0269c0cab6a07b3a
Khalid Sabbah <khalidsami.se@gmail.com>
2023-06-18 10:02:30 +0300

    debugging ..

--- UnitOne/aj_dashboard/backend/config/settings.py
+++ UnitOne/aj_dashboard/backend/config/settings.py
@@ -138,7 +138,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/django-static/"
-STATIC_ROOT = "static"
+STATIC_ROOT = "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

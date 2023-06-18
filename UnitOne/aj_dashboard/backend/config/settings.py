9eaab127e3d0d86b1ac6a789afe6e19adc32af5f
Khalid Sabbah <khalidsami.se@gmail.com>
2023-06-18 12:55:08 +0300

    debugging ..

--- UnitOne/aj_dashboard/backend/config/settings.py
+++ UnitOne/aj_dashboard/backend/config/settings.py
@@ -67,12 +67,12 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # use the CORS middleware
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000/",
-    "http://165.227.132.182/",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000/",
+#     "http://165.227.132.182/",
+# ]
 
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'config.urls'
 

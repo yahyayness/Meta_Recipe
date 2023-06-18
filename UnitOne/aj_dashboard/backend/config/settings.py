63dbf4357db9905c875749a5e3ce9cfefeaa1de7
Khalid Sabbah <khalidsami.se@gmail.com>
2023-06-18 09:03:56 +0300

    debugging static ..

--- UnitOne/aj_dashboard/backend/config/settings.py
+++ UnitOne/aj_dashboard/backend/config/settings.py
@@ -137,10 +137,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static', ]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

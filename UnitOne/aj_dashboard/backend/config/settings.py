8fa96d1c3288a7c5a5a8fa83d557d8b20ede9191
Khalid Sabbah <khalidsami.se@gmail.com>
2023-06-18 10:39:12 +0300

    debugging ..

--- UnitOne/aj_dashboard/backend/config/settings.py
+++ UnitOne/aj_dashboard/backend/config/settings.py
@@ -137,7 +137,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
